b92101fcb1c1d8d06aacc4004ba44bc5e8f3a735
ArvinTorabazari <arvintorabazari@gmail.com>
2025-04-26 18:09:19 -0400

    got the reddit table to work with hard coded date, missing yahoo finance link tho

--- assignment-2-stocks.js
+++ assignment-2-stocks.js
@@ -60,4 +60,62 @@ async function populateChart() {
     }
   });
 }
+async function bullOrBear() {
+  // Get todays date in milliseconds
+  const today = new Date();
 
+  // convert to seconds
+  const todayEpoch = Math.floor(today.getTime() / 1000);
+  console.log("Today's epoch:", todayEpoch)
+
+  // convert to ISO date for API
+  const formattedDate = new Date(todayEpoch * 1000).toISOString().split('T')[0];
+  console.log("Formatted Date (yyyy-mm-dd):", formattedDate);
+
+  const result = await fetch(`https://tradestie.com/api/v1/apps/reddit?date=2022-04-03`)
+    .then((response) => 
+    response.json());
+
+  console.log("reddit stock", result); 
+
+  const topStocks = result.slice(0, 5);
+  console.log("Top 5 Stocks", topStocks);
+
+  const tableBody = document.getElementById('bullishOrBearish').querySelector('tbody');
+  tableBody.innerHTML = "";
+
+  topStocks.forEach(stock => {
+    const row = document.createElement('tr');
+
+    const tickerRow = document.createElement('td');
+    tickerRow.textContent = stock.ticker; 
+
+    const commentRow = document.createElement('td');
+    commentRow.textContent = stock.no_of_comments; 
+
+    const sentimentRow = document.createElement('td');
+    const sentimentImage = document.createElement('img');
+    sentimentImage.width = 100;
+    sentimentImage.height = 100;
+
+    if (stock.sentiment.toLowerCase() === 'bullish') {
+      sentimentImage.src = 'bull.jpeg'; // Bull icon
+      sentimentImage.alt = 'Bullish';
+      sentimentRow.appendChild(sentimentImage);
+    } else if (stock.sentiment.toLowerCase() === 'bearish') {
+      sentimentImage.src = 'bear.jpeg'; // Bear icon
+      sentimentImage.alt = 'Bearish';
+      sentimentRow.appendChild(sentimentImage);
+    }
+    row.appendChild(tickerRow);
+    row.appendChild(commentRow);
+    row.appendChild(sentimentRow);
+
+    
+    tableBody.appendChild(row);
+  })
+}
+
+
+
+window.onload = bullOrBear;
